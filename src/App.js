529c18982db8b3a020f97ad967414d0ea6f30d15
cole <coleamurphy@gmail.com>
2024-08-14 17:03:53 -0400

    make first connection to MTAPI python server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {apiResponse: ""};
+  }
+
+  callAPI() {
+    fetch("http://127.0.0.1:5000")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>res: {this.state.apiResponse}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
