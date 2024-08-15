f103c3b631c9863a12a910adc650d5a0dda6ff91
cole <coleamurphy@gmail.com>
2024-08-15 12:27:11 -0400

    Create and start using fetchNearestStations api call

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from "react";
-import {fetchSubwayApi} from "./utils/subway_apis";
+import {fetchSubwayApi, fetchNearestStations} from "./utils/subway_apis";
 
 import './App.css';
 
+const SAMPLE_LAT = 40.849505;
+const SAMPLE_LON = -73.933596;
+
 function App() {
   const [res, setRes] = useState("");
+  const [nearest, setNearest] = useState("");
 
   useEffect(() => {
     fetchSubwayApi()
       .then(res => setRes(res));
+    fetchNearestStations(SAMPLE_LAT, SAMPLE_LON)
+      .then(res => setNearest(res));
   });
 
     return (
       <div className="App">
         <header className="App-header">
-          <p>res: {this.state.apiResponse}</p>
+          <p>res: {res}</p>
+          <p>res: {nearest}</p>
         </header>
       </div>
     );
--- src/utils/subway_apis.js
+++ src/utils/subway_apis.js
@@ -5,3 +5,9 @@ export const fetchSubwayApi = async () => {
       .then(res => res.text())
       .catch(error => console.error(error));
 }
+
+export const fetchNearestStations = async (latitude, longitude) => {
+  return await fetch(`${SUBWAY_API_URL}/by-location?lat=${latitude}&lon=${longitude}`)
+      .then(res => res.text())
+      .catch(error => console.error(error));
+}
