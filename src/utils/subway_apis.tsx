8d03864039bf2ac33a0f73f975b579d3bb984f6a
cole <coleamurphy@gmail.com>
2024-08-19 11:44:09 -0400

    Add all MTAPI APIs and json file of all stations

--- src/utils/subway_apis.tsx
+++ src/utils/subway_apis.tsx
@@ -1,4 +1,4 @@
-import { StationList } from "./interfaces";
+import { StationList, RouteList } from "./interfaces";
 
 const SUBWAY_API_URL = "http://127.0.0.1:5000";
 
@@ -10,6 +10,28 @@ export const fetchSubwayApi = async (): Promise < string | void > => {
 
 export const fetchNearestStations = async (latitude: number, longitude: number): Promise < StationList | void > => {
   return await fetch(`${SUBWAY_API_URL}/by-location?lat=${latitude}&lon=${longitude}`)
+    .then(res => res.text())
+    .then(text => (JSON.parse(text)))
+    .catch(error => console.error(error));
+}
+
+export const fetchRoute = async (route: string): Promise < StationList | void > => {
+  return await fetch(`${SUBWAY_API_URL}/by-route/${route}`)
+    .then(res => res.text())
+    .then(text => (JSON.parse(text)))
+    .catch(error => console.error(error));
+}
+
+export const fetchRoutes = async (): Promise < RouteList | void > => {
+  return await fetch(`${SUBWAY_API_URL}/routes`)
+    .then(res => res.text())
+    .then(text => (JSON.parse(text)))
+    .catch(error => console.error(error));
+}
+
+export const fetchStations = async (ids: string[]): Promise < StationList | void > => {
+  const idString = ids.join(",");
+  return await fetch(`${SUBWAY_API_URL}/by-id/${idString}`)
     .then(res => res.text())
     .then(text => (JSON.parse(text)))
     .catch(error => console.error(error));
