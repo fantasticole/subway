a34ca31b951e9e137a83f9c2afee43445bb8f1c9
cole <coleamurphy@gmail.com>
2024-08-30 18:53:54 -0400

    Get app fetching stations along route again

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 
 import {
   fetchRoute,
-  fetchRoutes,
 } from "./utils/subway_apis";
 import { Station as StationData, Route as RouteData } from "./utils/interfaces";
 
@@ -13,83 +12,35 @@ import Map, { highlightMap } from "./Map/Map";
 import './App.css';
 import './variables.css';
 
+const ALL_ROUTES = Object.values(RouteData);
+
 function App() {
-  const [getRoutes, setGetRoutes] = useState < boolean > (true);
   const [updated, setUpdated] = useState < string > ();
   const [stations, setStations] = useState < StationData[] > ();
-  const [routes, setRoutes] = useState < RouteData[] > ();
   const [highlights, setHighlights] = useState < highlightMap > ({});
   const [selectedRoute, setSelectedRoute] = useState < RouteData > (RouteData.A);
 
   useEffect(() => {
-    // if (getRoutes) {
-    //   // fetch all routes
-    //   fetchRoutes()
-    //     .then(res => {
-    //       if (res) {
-    //         setRoutes(res.data);
-    //         // If we have a response, we don't need to call this again
-    //         setGetRoutes(false);
-    //       }
-    //     });
-    // }
+    // fetch stations along selected route
+    fetchRoute(selectedRoute).then(res => {
+      if (res) {
+        setStations(res.data);
+        setUpdated((res.updated).toString());
+        setHighlights(() => (res.data || []).reduce((map, { id }) => {
+          map[id] = `train${selectedRoute} highlighted`;
+          return map;
+        }, {} as highlightMap), );
+      }
+    });
 
-    fetch("/").then(resOne => {
-      console.log({ resOne });
-      console.log({ body: resOne.body });
-      const reader = resOne.body ? resOne.body.getReader() : null;
-      console.log({ reader });
-      // return resOne.text();
-      // return resOne.text();
-      // }).then(text => {
-      //   console.log({ text });
-      //   return JSON.parse(text);
-      // }).then(sthn => {
-      // console.log({ sthn });
-    })
-
-    // fetch("/").then(resOne => {
-    //   console.log({ resOne });
-    //   return resOne.text();
-    // }).then(res => {
-    //   console.log({ res });
-    // })
-
-    fetch("/test").then(res => res.text()).then(text => {
-      console.log({ text });
-      //   return JSON.parse(text);
-      // }).then(test => {
-      //   console.log({ test });
-    })
-
-    // fetch("/test").then(res => res.text()).then(text => (JSON.parse(text))).then(test => {
-    //   console.log({ test });
-    // })
-
-    fetch("/stations").then(res => res.text()).then(text => (JSON.parse(text))).then(stations => {
-      console.log({ stations });
-    })
-
-    // // fetch stations along selected route
-    // fetchRoute(selectedRoute).then(res => {
-    //   if (res) {
-    //     setStations(res.data);
-    //     setUpdated(new Date(res.updated).toString());
-    //     setHighlights(() => (res.data || []).reduce((map, { id }) => {
-    //       map[id] = `train${selectedRoute} highlighted`;
-    //       return map;
-    //     }, {} as highlightMap), );
-    //   }
-    // });
-  }, [selectedRoute, getRoutes]);
-
+  }, [selectedRoute]);
 
   return (
     <div className="App">
         <h1>SUBWAY</h1>
         <h2>All routes</h2>
         <span data-testid="route-list">
-          {routes?.map((route: RouteData, i: number) => (
+          {ALL_ROUTES?.map((route: RouteData, i: number) => (
             <Route route={route}
                    key={i}
                    onClick={() => setSelectedRoute(route)} />
