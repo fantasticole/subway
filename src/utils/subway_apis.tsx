eb1b60e41c0c994b0d781709349b96ee661e99e5
cole <coleamurphy@gmail.com>
2024-08-31 00:17:22 -0400

    Create reusable callAPI method

--- src/utils/subway_apis.tsx
+++ src/utils/subway_apis.tsx
@@ -1,58 +1,37 @@
 import { StationList, RouteList, AllStationsResponse, ArrivalList, Train } from "./interfaces";
 
-export const fetchNearestStations = async (latitude: number, longitude: number): Promise < StationList | void > => {
-  return await fetch(`by-location?lat=${latitude}&lon=${longitude}`)
+async function callAPI(url: string): Promise < any > {
+  return await fetch(url)
     .then(res => res.json())
     .then((response) => {
       return { ...response, updated: new Date(response.updated) };
     })
     .catch(error => console.error(error));
+}
+
+export const fetchNearestStations = async (latitude: number, longitude: number): Promise < StationList | void > => {
+  return await callAPI(`by-location?lat=${latitude}&lon=${longitude}`)
 }
 
 export const fetchRoute = async (route: string): Promise < StationList | void > => {
-  return await fetch(`route/${route}`)
-    .then(res => res.json())
-    .then((response) => {
-      return { ...response, updated: new Date(response.updated) };
-    })
-    .catch(error => console.error(error));
+  return await callAPI(`route/${route}`)
 }
 
 export const fetchCurrentRoutes = async (): Promise < RouteList | void > => {
-  return await fetch('routes')
-    .then(res => res.json())
-    .then((response) => {
-      return { ...response, updated: new Date(response.updated) };
-    })
-    .catch(error => console.error(error));
+  return await callAPI('routes')
 }
 
 export const fetchStations = async (ids ? : string[]): Promise < StationList | void > => {
   const idString = ids && ids.length ? ids.join(",") : '';
-  return await fetch(`by-id/${idString}`)
-    .then(res => res.json())
-    .then((response) => {
-      return { ...response, updated: new Date(response.updated) };
-    })
-    .catch(error => console.error(error));
+  return await callAPI(`by-id/${idString}`)
 }
 
 export const fetchAllStations = async (): Promise < AllStationsResponse | void > => {
-  return await fetch('stations')
-    .then(res => res.json())
-    .then((response) => {
-      return { ...response, updated: new Date(response.updated) };
-    })
-    .catch(error => console.error(error));
+  return await callAPI('stations')
 }
 
 export const fetchArrivals = async (): Promise < ArrivalList | void > => {
-  return await fetch('arrivals')
-    .then(res => res.json())
-    .then((response) => {
-      return { ...response, updated: new Date(response.updated) };
-    })
-    .catch(error => console.error(error));
+  return await callAPI('arrivals')
 }
 
 export const fetchTrain = async (train: string): Promise < Train | void > => {
