17c0d9a6a1a9e6c9570dd62ffb2eedd1b5e9303a
cole <coleamurphy@gmail.com>
2024-09-12 13:53:19 -0400

    Create Train component

---
+++ src/Train/Train.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Train as TrainData, Location } from "../utils/interfaces";
+
+import Route from "../Route/Route";
+
+interface TrainParams {
+	train: TrainData;
+	position: Location | undefined;
+}
+
+function Train({ train, position }: TrainParams) {
+	// Render nothing if we don't have a position for this train
+	if (!position) return;
+
+	const style = {
+		bottom: position[0],
+		left: position[1],
+	};
+
+	return (
+		<span style={style} className="train" title={train.summary}>
+			<Route route={train.route}
+		         data-testid="Train"/>
+		</span>
+	);
+
+}
+
+export default Train;
