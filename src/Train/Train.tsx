e0403f0b9fb79b05a4a472f3463ebd0da93072ba
cole <coleamurphy@gmail.com>
2024-09-23 14:10:21 -0400

    Only animate train movement if the trip ID hasn't changed

--- src/Train/Train.tsx
+++ src/Train/Train.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useMemo } from "react";
 
 import { Train as TrainData, Location } from "../utils/interfaces";
 
@@ -12,9 +12,19 @@ interface TrainParams {
 }
 
 function Train({ train, position }: TrainParams) {
+	const prevTrain = useRef < TrainData > ();
+
+	const sameTrain: boolean = useMemo(
+		() => (!!prevTrain.current && train.trip_id === prevTrain.current.trip_id),
+		[train, prevTrain]
+	);
+
+	const trainTransition = sameTrain ? 'bottom 1s ease-in-out, left 1s ease-in-out' : '';
+
 	const style = {
 		bottom: position[0],
 		left: position[1],
+		transition: trainTransition,
 	};
 
 	return (
